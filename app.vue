3d585803ded0568490c5779954864b718806481c
gyorgy.varga <gyorgy.varga@icellmobilsoft.hu>
2022-02-19 16:59:52 +0100

    loading és scroll fix

--- app.vue
+++ app.vue
@@ -1,5 +1,25 @@
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <div class="scroll-smooth">
+    <nuxt-loading ref="loading" />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
+
+<script setup>
+// TODO: ideiglenes megoldás, ameddig nincs benne natívban a Nuxt 3- ban
+import NuxtLoading from './components/common/nuxt-loading'
+
+const loading = ref(null)
+const nuxtApp = useNuxtApp()
+
+nuxtApp.hook('page:start', () => {
+  loading.value.start()
+})
+
+nuxtApp.hook('page:finish', () => {
+  loading.value.finish()
+  window.scrollTo(0, 0)
+})
+</script>
