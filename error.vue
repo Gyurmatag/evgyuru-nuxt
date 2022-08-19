88a1fea9876f7b0b7165eb01f68c9316409f3408
gyorgy.varga <gyorgy.varga@icellmobilsoft.hu>
2022-08-19 16:44:18 +0200

    error page fejlesztése

---
+++ error.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="scroll-smooth dark:bg-gray-800">
+    <common-page-head
+      :subtitle="error.satusCode === '404' ? 'error.404' : error.message"
+    ></common-page-head>
+    <NuxtLayout>
+      <div v-if="error.statusCode === '404'">{{ $t("error.404") }}</div>
+      <div v-else>{{ $t(error.message) }}</div>
+      <button @click="handleError">{{ $t("error.backToHomePage") }}</button>
+    </NuxtLayout>
+  </div>
+</template>
+
+<script setup lang="ts">
+// TODO: type-osítás, default value adás
+defineProps({
+  error: Object,
+});
+
+const handleError = () => clearError({ redirect: "/" });
+</script>
--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,12 +1,14 @@
 import { useUserStore } from "~/stores/user";
 
-export default defineNuxtRouteMiddleware((to) => {
+export default defineNuxtRouteMiddleware((to, from) => {
   const userStore = useUserStore();
+
+  console.log(userStore.user.accessToken);
 
   if (userStore.user.accessToken) {
     // TODO: ezt megkérdezni, hogy miért csak így működik githubon?
-    navigateTo(to);
+    console.log(to);
     return;
   }
-  return navigateTo("/");
+  return abortNavigation("error.notAuthenticatedOrAuthorized");
 });
