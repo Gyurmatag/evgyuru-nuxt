3b4c42c98032f02271bbe8701973ca8bb1ad2f21
Varga György <gyurmatag@gmail.com>
2022-08-20 15:11:24 +0200

    error page formázása + nuxt verziók

--- error.vue
+++ error.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="scroll-smooth dark:bg-gray-800">
     <common-page-head
-      :subtitle="error.satusCode === '404' ? 'error.404' : error.message"
+      :subtitle="error.statusCode === '404' ? 'error.404' : error.message"
     ></common-page-head>
     <NuxtLayout>
-      <div v-if="error.statusCode === '404'">{{ $t("error.404") }}</div>
-      <div v-else>{{ $t(error.message) }}</div>
-      <button @click="handleError">{{ $t("error.backToHomePage") }}</button>
+      <div class="flex flex-col items-center text-gray-800 dark:text-gray-200">
+        <div class="my-8 flex max-w-6xl flex-col space-y-4">
+          <div class="text-center text-5xl">:(</div>
+          <div v-if="error.statusCode === '404'" class="text-center">
+            {{ $t("error.404") }}
+          </div>
+          <div v-else>{{ $t(error.message) }}</div>
+          <button
+            class="rounded-md border border-blue-400 p-2 transition duration-300 ease-in-out hover:bg-gray-50 dark:hover:bg-gray-600"
+            @click="handleError"
+          >
+            {{ $t("error.backToHomePage") }}
+          </button>
+        </div>
+      </div>
     </NuxtLayout>
   </div>
 </template>
